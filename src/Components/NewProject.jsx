a31565e3e00cc1657bec4692aceca9a6988dc9df
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-10-03 03:27:02 +0300

    Create NewProject.jsx

---
+++ src/Components/NewProject.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const NewProject = () => {
+  return (
+    <div className="">
+      <menu className="">
+        <li className="">
+          <button className="">Cancel</button>
+          <button className="">Save</button>
+        </li>
+      </menu>
+
+      <div className=""></div>
+    </div>
+  );
+};
+
+export default NewProject;
