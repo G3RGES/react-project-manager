1be6422d8abcd6e006bf756a87addc5f45ab6729
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-10-04 07:40:42 +0300

    Update NoProjectSelected.jsx

--- src/Components/NoProjectSelected.jsx
+++ src/Components/NoProjectSelected.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import noProject from "../assets/no-projects.png";
+import Button from "./Button";
 
 const NoProjectSelected = () => {
-  return <div>NoProjectSelected</div>;
+  return (
+    <div className="mt-24 text-center w-2/3 ">
+      <img
+        className="w-16 h-16 object-contain mx-auto "
+        src={noProject}
+        alt="no-project"
+      />
+      <h2 className="text-xl font-bold text-stone-500 my-4  ">
+        No project selected
+      </h2>
+      <p className="text-stone-400 mb-4 ">
+        Select a project, or get started with a new one
+      </p>
+      <p className="mt-8">
+        <Button>Create new project</Button>
+      </p>
+    </div>
+  );
 };
 
 export default NoProjectSelected;
