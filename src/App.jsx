91eff2c626b58cb16fb471fcfb34796ee1a6e473
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-10-04 08:43:35 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+import { useState } from "react";
 import NewProject from "./Components/NewProject";
+import NoProjectSelected from "./Components/NoProjectSelected";
 import ProjectsSidebar from "./Components/ProjectsSidebar";
 
+const projects = {
+  selectedProjectId: undefined,
+  projects: [],
+};
+
 function App() {
+  const [projectsState, setProjectsState] = useState(projects);
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectsSidebar />
-      <NewProject />
+      {/* <NewProject /> */}
+      <NoProjectSelected />
     </main>
   );
 }
