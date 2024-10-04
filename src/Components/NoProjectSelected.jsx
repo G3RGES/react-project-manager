c440dcf5b1adc9061a5f3ce00e5c11d475a7c389
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-10-04 08:54:58 +0300

    Update NoProjectSelected.jsx

--- src/Components/NoProjectSelected.jsx
+++ src/Components/NoProjectSelected.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import noProject from "../assets/no-projects.png";
 import Button from "./Button";
 
-const NoProjectSelected = () => {
+const NoProjectSelected = ({ onAddProject }) => {
   return (
     <div className="mt-24 text-center w-2/3 ">
       <img
@@ -17,7 +17,7 @@ const NoProjectSelected = () => {
         Select a project, or get started with a new one
       </p>
       <p className="mt-8">
-        <Button>Create new project</Button>
+        <Button onClick={onAddProject}>Create new project</Button>
       </p>
     </div>
   );
