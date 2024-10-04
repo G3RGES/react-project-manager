324e5fecb67b53429e7e8a59f21cabfedf9645fe
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-10-04 08:54:54 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,28 @@ const projects = {
 
 function App() {
   const [projectsState, setProjectsState] = useState(projects);
+
+  const handleAddProject = () => {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  };
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onAddProject={handleAddProject} />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar />
-      {/* <NewProject /> */}
-      <NoProjectSelected />
+      <ProjectsSidebar onAddProject={handleAddProject} />
+      {content}
     </main>
   );
 }
