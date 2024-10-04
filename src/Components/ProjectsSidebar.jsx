0dfb6b16aa8277927085b8273e5d5cbe4aa66ee3
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-10-04 12:01:05 +0300

    Update ProjectsSidebar.jsx

--- src/Components/ProjectsSidebar.jsx
+++ src/Components/ProjectsSidebar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Button from "./Button";
 
-const ProjectsSidebar = ({ onAddProject }) => {
+const ProjectsSidebar = ({ onAddProject, projects }) => {
   return (
     <aside
       className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 
@@ -14,7 +14,18 @@ const ProjectsSidebar = ({ onAddProject }) => {
         <Button onClick={onAddProject}>+ Add project</Button>
       </div>
 
-      <ul className=""></ul>
+      <ul className="mt-8">
+        {projects.map((project) => (
+          <li key={project.id}>
+            <button
+              className="w-full text-left px-2 py-1 rounded-sm my-1
+            text-stone-400 hover:text-stone-200 hover:bg-stone-800"
+            >
+              {project.title}
+            </button>
+          </li>
+        ))}
+      </ul>
     </aside>
   );
 };
