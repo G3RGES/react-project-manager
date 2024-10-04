02cb7cf53eeb6c13987335f0ace434036b0d4303
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-10-04 11:21:10 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -44,6 +44,8 @@ function App() {
     content = <NoProjectSelected onAddProject={handleCreateProject} />;
   }
 
+  console.log(projectsState);
+
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectsSidebar onAddProject={handleCreateProject} />
