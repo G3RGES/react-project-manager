f23ef6438b11ee184ea777439fe013ea2bc0b0b6
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-10-05 11:58:20 +0300

    Update Modal.jsx

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useImperativeHandle, forwardRef, useRef } from "react";
+import { createPortal } from "react-dom";
 
-const Modal = () => {
-  return <div>Modal</div>;
-};
+const Modal = forwardRef(({ children }, ref) => {
+  const dialog = useRef();
+
+  useImperativeHandle(ref, () => {
+    return {
+      open() {},
+    };
+  });
+
+  return createPortal(
+    <dialog className="">{children}</dialog>,
+    document.getElementById("modal-root")
+  );
+});
 
 export default Modal;
