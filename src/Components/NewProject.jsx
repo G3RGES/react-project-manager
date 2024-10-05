c437426fd3efef111aa3a5b98ec19fe4ad2eb692
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-10-05 12:04:27 +0300

    Update NewProject.jsx

--- src/Components/NewProject.jsx
+++ src/Components/NewProject.jsx
@@ -1,10 +1,12 @@
 import React, { useRef } from "react";
 import Input from "./Input";
+import Modal from "./Modal";
 
 const NewProject = ({ onCreateProject }) => {
   const title = useRef();
   const description = useRef();
   const dueDate = useRef();
+  const modal = useRef();
 
   const handleSave = () => {
     const enteredTitle = title.current.value;
@@ -16,7 +18,8 @@ const NewProject = ({ onCreateProject }) => {
       enteredDescription.trim() === "" ||
       enteredDueDate.trim() === ""
     ) {
-      // show error modal
+      modal.current.open();
+      return;
     }
 
     onCreateProject({
@@ -27,30 +30,33 @@ const NewProject = ({ onCreateProject }) => {
   };
 
   return (
-    <div className="w-[35rem] mt-16  ">
-      <menu className="flex items-center justify-end gap-4 my-4">
-        <li className="">
-          <button className="uppercase text-stone-800 hover:text-stone-950">
-            Cancel
-          </button>
-        </li>
-        <li className="">
-          <button
-            onClick={handleSave}
-            className="uppercase px-6 py-2 rounded-md bg-stone-800
+    <>
+      <Modal ref={modal} />
+      <div className="w-[35rem] mt-16  ">
+        <menu className="flex items-center justify-end gap-4 my-4">
+          <li className="">
+            <button className="uppercase text-stone-800 hover:text-stone-950">
+              Cancel
+            </button>
+          </li>
+          <li className="">
+            <button
+              onClick={handleSave}
+              className="uppercase px-6 py-2 rounded-md bg-stone-800
             text-stone-50 hover:bg-stone-950 "
-          >
-            Save
-          </button>
-        </li>
-      </menu>
+            >
+              Save
+            </button>
+          </li>
+        </menu>
 
-      <div className="">
-        <Input label="Title" ref={title} type="text" />
-        <Input label="Description" textarea ref={description} />
-        <Input label="Due Date" type="date" ref={dueDate} />
+        <div className="">
+          <Input label="Title" ref={title} type="text" />
+          <Input label="Description" textarea ref={description} />
+          <Input label="Due Date" type="date" ref={dueDate} />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
