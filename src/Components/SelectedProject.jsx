fecfc56562c460e23386675f13cdfc6bb72ac993
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-10-05 13:28:15 +0300

    Create SelectedProject.jsx

---
+++ src/Components/SelectedProject.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Button from "./Button";
+
+const SelectedProject = () => {
+  return (
+    <div className="">
+      <header className="">
+        <div className="">
+          <h1 className="">TITLE</h1>
+          <Button>Delete</Button>
+        </div>
+      </header>
+
+      {/* TASKS */}
+    </div>
+  );
+};
+
+export default SelectedProject;
