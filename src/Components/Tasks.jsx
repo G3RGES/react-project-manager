4a0087dff43529f13d4125cda3571218ff60129b
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-10-06 10:31:25 +0300

    Update Tasks.jsx

--- src/Components/Tasks.jsx
+++ src/Components/Tasks.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import NewTask from "./NewTask";
 
-const Tasks = () => {
+const Tasks = ({ addTask, deleteTask }) => {
   return (
     <section className="">
       <h2 className="text-2xl font-bold text-stone-700 mb-4">Tasks</h2>
-      <NewTask />
+      <NewTask addTask={addTask} />
       <p className="text-stone-800 my-4">This project doesn't have any tasks</p>
       <ul className=""></ul>
     </section>
