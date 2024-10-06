3ae2154232b39b8a11b3ce079d2c719edc221ae3
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-10-06 09:17:46 +0300

    Update Tasks.jsx

--- src/Components/Tasks.jsx
+++ src/Components/Tasks.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import NewTask from "./NewTask";
 
 const Tasks = () => {
-  return <div>Tasks</div>;
+  return (
+    <section className="">
+      <h2 className="text-2xl font-bold text-stone-700 mb-4">Tasks</h2>
+      <NewTask />
+      <p className="text-stone-800 mb-4">This project doesn't have any tasks</p>
+      <ul className=""></ul>
+    </section>
+  );
 };
 
 export default Tasks;
