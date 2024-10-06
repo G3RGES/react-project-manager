bd23df1d7d0c7610d2b05d5fa1d3219c35421b85
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-10-06 10:31:21 +0300

    Update NewTask.jsx

--- src/Components/NewTask.jsx
+++ src/Components/NewTask.jsx
@@ -1,25 +1,33 @@
 import React, { useState } from "react";
 
-const NewTask = () => {
-  const [tasks, setTasks] = useState();
+const NewTask = ({ addTask }) => {
+  const [enteredTask, setEnteredTask] = useState("");
 
   const handleChange = (event) => {
     setTasks(event.target.value);
   };
 
-  console.log(tasks);
+  const handleClick = () => {
+    addTask(enteredTask);
+    setEnteredTask("");
+  };
 
   return (
     <div className="flex items-center justify-between">
       <input
         onChange={handleChange}
-        value={tasks}
+        value={enteredTask}
         className="focus:outline-none border-b-2 rounded-sm
          border-stone-300 w-64 px-2 py-1 bg-stone-200"
         type="text"
         placeholder="Add new task to you project"
       />
-      <button className="text-stone-700 hover:text-stone-950">Add Task</button>
+      <button
+        className="text-stone-700 hover:text-stone-950"
+        onClick={handleClick}
+      >
+        Add Task
+      </button>
     </div>
   );
 };
