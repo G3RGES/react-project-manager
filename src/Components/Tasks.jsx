83b75b24a721f799accd3ed22b926ee236d94f37
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-10-06 20:20:29 +0300

    Update Tasks.jsx

--- src/Components/Tasks.jsx
+++ src/Components/Tasks.jsx
@@ -6,8 +6,28 @@ const Tasks = ({ tasks, addTask, deleteTask }) => {
     <section className="">
       <h2 className="text-2xl font-bold text-stone-700 mb-4">Tasks</h2>
       <NewTask addTask={addTask} />
-      <p className="text-stone-800 my-4">This project doesn't have any tasks</p>
-      <ul className=""></ul>
+      {tasks.length === 0 ? (
+        <p className="text-stone-800 my-4">
+          This project doesn't have any tasks
+        </p>
+      ) : (
+        <ul className="p-4 mt-8 rounded-md bg-stone-100">
+          {tasks.map((task) => (
+            <li
+              className="flex items-center justify-between my-4"
+              key={task.id}
+            >
+              <span className="">{task.text}</span>
+              <button
+                className="text-stone-700 hover:text-red-500"
+                onClick={deleteTask}
+              >
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
     </section>
   );
 };
